a38a48f977b5db2f80a36cd7e0ecde6c0ae3598b
Alex Kulcsar <38046796+alex-kulcsar@users.noreply.github.com>
2020-08-23 13:43:44 -0400

    Finish primary transforms

--- main.ts
+++ main.ts
@@ -6,6 +6,7 @@ moveSprite.y = 4
 moveSprite.setFlag(SpriteFlag.Ghost, true)
 
 let currMove: CubeTransform = CubeTransform.Front
+let moves: string = ''
 writeCurrMove()
 
 let myCube: cube.Cube = cube.buildCube(3)
@@ -13,14 +14,14 @@ let myCube: cube.Cube = cube.buildCube(3)
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     currMove += -1
     if (currMove == CubeTransform.None) {
-        currMove = CubeTransform.Standing
+        currMove = CubeTransform.Right
     }  // if (currMove === CubeTransform.None)
     writeCurrMove()
 })
 
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     currMove += 1
-    if (currMove == CubeTransform.InsideFront) {
+    if (currMove == CubeTransform.RotateX) {
         currMove = CubeTransform.Front
     }  // if (currMove === CubeTransform.InsideFront)
     writeCurrMove()
@@ -28,14 +29,31 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
 
 controller.right.onEvent(ControllerButtonEvent.Pressed, function() {
     myCube.move({transform: currMove, inverse: false})
+    addCurrMove(false)
 })
 
 controller.left.onEvent(ControllerButtonEvent.Pressed, function() {
     myCube.move({transform: currMove, inverse: true})
+    addCurrMove(true)
 })
+
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    game.showLongText(moves, DialogLayout.Center)
+})
+
+function addCurrMove(inverse: boolean): void {
+    if (moves.length === 0) {
+        moves = cube.MOVE_NAMES[currMove][1]
+    } else {
+        moves += ' ' + cube.MOVE_NAMES[currMove][1]
+    }   // if (!moves)
+    if (inverse) {
+        moves += "'"
+    }   // if (inverse)
+}   // addCurrMove()
 
 function writeCurrMove () {
     moveImage.fill(0)
-    let m: string = cube.MOVE_NAMES[currMove]
+    let m: string = cube.MOVE_NAMES[currMove][0]
     moveImage.printCenter(m, 1, 5, image.font8)
 }   // writeCurrMove()
